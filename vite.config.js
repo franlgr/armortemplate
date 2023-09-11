8a008cda65efc384f50d45b6e15404b7b20ee757
CharlyG <carlo.gammarota@gmail.com>
2023-09-11 18:33:28 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue';export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, '/src'),
+      '@': '/src',
     },
   },
 });
