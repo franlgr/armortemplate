0fcaa213a6c90adc760a1415f5a964bee02dec4e
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-09-11 18:50:31 -0300

    update

---
+++ src/views/admin/TableDefault.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="overflow-x-auto">
+        <table class="table">
+            <!-- head -->
+            <thead>
+                <tr>
+                    <th>
+                        <label>
+                    <input type="checkbox" class="checkbox" />
+                  </label>
+                    </th>
+                    <th>Name</th>
+                    <th>Job</th>
+                    <th>Favorite Color</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- row 1 -->
+                <tr>
+                    <th>
+                        <label>
+                    <input type="checkbox" class="checkbox" />
+                  </label>
+                    </th>
+                    <td>
+                        <div class="flex items-center space-x-3">
+                            <div class="avatar">
+                                <div class="mask mask-squircle w-12 h-12">
+                                    <img src="https://picsum.photos/300/300" alt="Avatar Tailwind CSS Component" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="font-bold">Hart Hagerty</div>
+                                <div class="text-sm opacity-50">United States</div>
+                            </div>
+                        </div>
+                    </td>
+                    <td>
+                        Zemlak, Daniel and Leannon
+                        <br/>
+    
+                    </td>
+                    <td>Purple</td>
+                    <th>
+                        <button class="btn btn-sm border-solid border-black bg-green-400">show</button>
+                        <button class="btn btn-sm border-solid border-black bg-blue-400 mx-2">edit</button>
+                        <button class="btn btn-sm border-solid border-black bg-red-400">delete</button>
+                    </th>
+                </tr>
+            </tbody>
+            <!-- foot -->
+            <!-- <tfoot>
+                                    <tr>
+                                        <th></th>
+                                        <th>Name</th>
+                                        <th>Job</th>
+                                        <th>Favorite Color</th>
+                                        <th></th>
+                                    </tr>
+                                </tfoot> -->
+    
+        </table>
+    </div>
+</template>
