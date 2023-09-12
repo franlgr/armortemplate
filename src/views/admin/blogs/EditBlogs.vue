4613033d970b919e961c4d0558dc702595001b37
franlgr <grilfran90@gmail.com>
2023-09-12 19:56:39 -0300

    update

--- src/views/admin/blogs/EditBlogs.vue
+++ src/views/admin/blogs/EditBlogs.vue
@@ -4,6 +4,35 @@
         <div class="p-4">
             {{ blogs }}
         </div>
+        <div class=" m-4 2xl:container">
+                <div class="">
+                    Text Editor
+                    <br>
+                    <!-- <Ckeditor></Ckeditor>
+                    <br> Tabla Default
+                    <TableDefault></TableDefault> -->
+                    <br>
+                    Card Modelo Example
+                    <div class="card w-96 bg-base-100 shadow-xl my-8">
+                        <figure><img :src="blogs.image " alt="Shoes" /></figure>
+                        <div class="card-body">
+                            <h2 class="card-title">
+                                {{blogs.title}}
+                                <div class="badge badge-secondary">NEW</div>
+                            </h2>
+                            <p>{{ blogs.content }}</p>
+                            <div class="card-actions justify-end">
+                                <div class="badge badge-outline">Fashion</div>
+                                <div class="badge badge-outline">Products</div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+    
+                    <!-- Se pueden buscar componentes en https://daisyui.com/components/ -->
+    
+                </div>
+            </div>
     </div>
 </template>
 
