e69bb94e044796021a38457ceef3e3e274973fe4
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-09-12 20:20:12 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from "url";
 export default defineConfig({
   plugins: [
     vue(),
     ckeditor5({ theme: require.resolve('@ckeditor/ckeditor5-theme-lark') }),
   ],
   resolve: {
-    alias: {
-      '@': '/src',
-    },
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/shared/assets', import.meta.url)) },
+      { find: '@cmp', replacement: fileURLToPath(new URL('./src/shared/cmp', import.meta.url)) },
+      { find: '@stores', replacement: fileURLToPath(new URL('./src/shared/stores', import.meta.url)) },
+      { find: '@use', replacement: fileURLToPath(new URL('./src/shared/use', import.meta.url)) },
+    ]
   },
 });
