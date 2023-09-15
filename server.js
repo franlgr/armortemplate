96641455618289035bc2ef2668cdaedc2af7fa01
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-09-14 22:09:51 -0300

    update

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '/dist'), staticOptions));
 
 // Iniciar el servidor
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2222;
 server.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
