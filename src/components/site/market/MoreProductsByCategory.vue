f6fcc3f379a8779bfe80761badbc3e251dfcff88
CharlyG <carlo.gammarota@gmail.com>
2023-09-24 05:38:59 -0300

    'update'

--- src/components/site/market/MoreProductsByCategory.vue
+++ src/components/site/market/MoreProductsByCategory.vue
@@ -4,47 +4,51 @@ atego
     x-data="{ cartOpen: false , isOpen: false }"
     class="bg-white m-4 rounded-md"
   >
-    <main class="my-8 py-8" v-if="category">
+    <main class="my-8 py-8" v-if="category && products.length > 1">
       <div class="container mx-auto px-6">
         <div class="">
           <h3 class="text-gray-600 text-2xl font-medium">
             More Products By {{ category.title }}
           </h3>
+          <!--  -->
           <div
             class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
           >
-            <div
-              v-for="product in products"
-              :key="product.index"
-              class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
-            >
+            <div v-for="product in products" :key="product.index">
               <div
-                class="flex items-end justify-end h-56 w-full bg-cover"
-                :style="{
-                  'background-image': 'url(' + product.images[0] + ')',
-                }"
+                @click="single(product._id)"
+                class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden"
               >
-                <button
-                  class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+                <div
+                  class="flex items-end justify-end h-56 w-full bg-cover"
+                  :style="{
+                    'background-image': 'url(' + product.images[0] + ')',
+                  }"
                 >
-                  <svg
-                    class="h-5 w-5"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+                  <button
+                    class="p-2 rounded-full bg-blue-600 text-white mx-5 -mb-4 hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
                   >
-                    <path
-                      d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-              <div class="px-5 py-3 mt-2">
-                <h3 class="text-gray-700 uppercase">{{ product.title }}</h3>
-                <span class="text-gray-500 mt-2">u$s {{ product.price }}</span>
+                    <svg
+                      class="h-5 w-5"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+                <div class="px-5 py-3 mt-2">
+                  <h3 class="text-gray-700 uppercase">{{ product.title }}</h3>
+                  <span class="text-gray-500 mt-2"
+                    >u$s {{ product.price }}</span
+                  >
+                </div>
               </div>
             </div>
           </div>
@@ -76,6 +80,9 @@ atego
     },
     methods: {
       ...mapActions(['loadingSet']),
+      single(id) {
+        this.$router.push({ name: 'site-product', params: { id } });
+      },
       async getProducts() {
         try {
           if (!this.category) {
@@ -83,6 +90,7 @@ atego
             const response = await FeathersClient.service('products').find({
               query: {
                 $limit: 4,
+                //
                 // 'category._id': this.$route.
                 //route params
                 $sort: {
@@ -95,15 +103,18 @@ atego
           const response = await FeathersClient.service('products').find({
             query: {
               $limit: 4,
-              // 'category._id': this.$route.
-              //route params
+              //this.$route.params.id,
               'category._id': this.category._id,
               $sort: {
                 createdAt: -1,
               },
             },
           });
-          this.products = response.data;
+
+          this.products = response.data.filter(
+            (producto) => producto._id !== this.$route.params.id,
+          );
+          // this.products = response.data;
           this.loadingSet(false);
         } catch (error) {
           this.loadingSet(false);
