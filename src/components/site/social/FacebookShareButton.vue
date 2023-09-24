1d61cef73ed0c3139c8d537b032bd73d15dcaebc
CharlyG <carlo.gammarota@gmail.com>
2023-09-23 23:39:22 -0300

    'update'

---
+++ src/components/site/social/FacebookShareButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="fb-share-button">
+      <a target="_blank" :href="shareUrl" class="fb-xfbml-parse-ignore">
+        <i class="fab fa-facebook text-5xl ml-4"></i>
+        <!-- <span class="text-sm pl-2 pb-2 p-0">Compartir</span> -->
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      shareUrl() {
+        // Aquí puedes determinar la URL actual de la página
+        // y formatearla como lo necesitas para compartir en Facebook
+        const currentUrl = window.location.href;
+        const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+          currentUrl,
+        )}&src=sdkpreparse`;
+        return facebookShareUrl;
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  /* Estilos para tu componente */
+</style>
